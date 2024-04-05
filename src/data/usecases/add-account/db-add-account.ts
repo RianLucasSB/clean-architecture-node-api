983ab073dd63f905bb0618a12eee2fdde2443755
Rian Lucas <rianlucas2014@gmail.com>
2024-04-05 13:04:07 -0300

    feat: ensure DbAddAccount returns an account on success

--- src/data/usecases/add-account/db-add-account.ts
+++ src/data/usecases/add-account/db-add-account.ts
@@ -11,8 +11,7 @@ export class DbAddAcount implements AddAccount {
 
   async add (accountData: AddAccountModel): Promise<AccountModel> {
     const hashedPassword = await this.encrypter.encrypt(accountData.password)
-    await this.addAccountRepository.add(Object.assign({}, accountData, { password: hashedPassword }))
-    // eslint-disable-next-line @typescript-eslint/consistent-type-assertions
-    return await new Promise(resolve => { resolve({} as AccountModel) })
+    const account = await this.addAccountRepository.add(Object.assign({}, accountData, { password: hashedPassword }))
+    return account
   }
 }
